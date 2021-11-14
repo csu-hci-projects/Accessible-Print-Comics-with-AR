0f9eb0256607be5886fc42de86b5532f73967462
Brian T. Sullivan <obibrian@gmail.com>
2021-11-13 22:32:16 -0700

    Adding basic site to use as the "application"
    Since my experiment relies on a Wizard of Oz design, there is relatively little in the way of coding.

---
+++ AccessibleComics-App/IncreaseComic4.php
@@ -0,0 +1,98 @@
+<?php
+	$title = "Accessible Comics";
+	include "Include/ACheader.php";
+?>
+
+<body>
+
+<!-- 	Exit -->
+	<div>
+		<div id="mainCarousel" class="carousel slide">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#mainCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#mainCarousel" data-slide-to="1"></li>
+				<li data-target="#mainCarousel" data-slide-to="2"></li>
+				<li data-target="#mainCarousel" data-slide-to="3"></li>
+				<li data-target="#mainCarousel" data-slide-to="4"></li>
+				<li data-target="#mainCarousel" data-slide-to="5"></li>
+				<li data-target="#mainCarousel" data-slide-to="6"></li>
+				<li data-target="#mainCarousel" data-slide-to="7"></li>
+				<li data-target="#mainCarousel" data-slide-to="8"></li>
+				<li data-target="#mainCarousel" data-slide-to="9"></li>
+				<li data-target="#mainCarousel" data-slide-to="10"></li>
+				<li data-target="#mainCarousel" data-slide-to="11"></li>
+			</ol>
+			<center>
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="Media/Increase/Comic4/Comic4-Increase0.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase1.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase2.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase3.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase4.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase5.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase6.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase7.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase8.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase9.jpg">
+				</div>
+			
+				<div class="item">
+					<img src="Media/Increase/Comic4/Comic4-Increase10.jpg">
+				</div>
+				
+				<div class="item">
+					<img src="Media/CompleteComic.jpg">
+				</div>
+		
+			</div>
+			</center>
+			<!-- Left & Right Controls -->
+			<a class="left carousel-control" href="#mainCarousel" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#mainCarousel" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
+	
+	
+	<br>
+	<br>
+	<center>
+		<a href="index.php" alt="home">Back</a>
+	</center>
+	
+</body>
